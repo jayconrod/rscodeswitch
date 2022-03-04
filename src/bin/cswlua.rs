66ff4f9385016f23078e117d1552aa77229b851e
Jay Conrod <jayconrod@gmail.com>
2022-03-04 14:40:22 -0800

    cswlua: add -i option for REPL

--- src/bin/cswlua.rs
+++ src/bin/cswlua.rs
@@ -1,34 +1,46 @@
 use codeswitch::interpret::{Env, InterpreterFactory};
 use codeswitch::lua::compile;
 use codeswitch::lua::luastd;
+use codeswitch::lua::scope;
+use codeswitch::lua::syntax;
+use codeswitch::lua::token;
+use codeswitch::pos::{Error, LineMap};
 use codeswitch::runtime::{PackageLoader, ProvidedPackageSearcher};
 
 use std::cell::RefCell;
-use std::error::Error;
 use std::fmt::{self, Display, Formatter};
 use std::io;
 use std::path::PathBuf;
 use std::process;
 
 use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 #[derive(Parser, Debug)]
 struct Args {
     #[clap(short, long, help = "Disassemble and print the compiled package")]
     disassemble: bool,
 
-    path: String,
+    #[clap(short, long, help = "Run interactively.")]
+    interactive: bool,
+
+    paths: Vec<String>,
 }
 
 fn main() {
     let args = Args::parse();
     if let Err(err) = run(&args) {
-        eprintln!("{}", err);
+        eprintln!("cswlua: {}", err);
         process::exit(1);
     }
 }
 
-fn run(args: &Args) -> Result<(), Box<dyn Error>> {
+fn run(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+    if !args.interactive && args.paths.is_empty() {
+        return Err(Box::new(StringError("at least one argument is required")));
+    }
+
     let mut searcher = Box::new(ProvidedPackageSearcher::new());
     let std_package = luastd::build_std_package();
     if args.disassemble {
@@ -39,15 +51,6 @@ fn run(args: &Args) -> Result<(), Box<dyn Error>> {
     }
     searcher.add(std_package);
 
-    let path = PathBuf::from(&args.path);
-    let package = compile::compile_file(&path)?;
-    if args.disassemble {
-        print!(
-            "-- begin disassembly: {} --\n{}\n-- end disassembly --\n",
-            package.name, package
-        );
-    }
-
     let loader_cell = RefCell::new(PackageLoader::new(searcher));
     let mut input = io::stdin();
     let mut output = io::stdout();
@@ -56,11 +59,93 @@ fn run(args: &Args) -> Result<(), Box<dyn Error>> {
         w: &mut output,
     });
     let interp_fac = InterpreterFactory::new(&env_cell);
-    let res = unsafe { PackageLoader::load_given_package(&loader_cell, interp_fac, package) };
-    match res {
-        Ok(_) => Ok(()),
-        Err(err) => Err(Box::new(err)),
+
+    for path_arg in &args.paths {
+        let path = PathBuf::from(&path_arg);
+        let package = compile::compile_file(&path)?;
+        if args.disassemble {
+            print!(
+                "-- begin disassembly: {} --\n{}\n-- end disassembly --\n",
+                package.name, package
+            );
+        }
+        let res = unsafe { PackageLoader::load_given_package(&loader_cell, interp_fac, package) };
+        if let Err(err) = res {
+            return Err(Box::new(err));
+        }
     }
+
+    if args.interactive {
+        let mut buf = Vec::new();
+        let mut rl = Editor::<()>::new();
+        let path = PathBuf::from("<stdin>");
+        let name = String::from("<stdin>");
+
+        let is_incomplete = |errors: &[Error]| {
+            if errors.len() != 1 {
+                return false;
+            }
+            let err = &errors[0];
+            // TODO: figure out some other way to indicate an early EOF.
+            // Depending on the error message is too brittle.
+            err.message.ends_with(", but found end of file")
+        };
+
+        loop {
+            // Read the next line from the user.
+            let prompt = if buf.is_empty() { "> " } else { ">> " };
+            let line = match rl.readline(prompt) {
+                Ok(line) => line,
+                Err(ReadlineError::Eof) => break,
+                Err(ReadlineError::Interrupted) => continue,
+                Err(err) => {
+                    eprintln!("{}", err);
+                    continue;
+                }
+            };
+            buf.extend_from_slice(line.as_bytes());
+
+            // Attempt to parse.
+            let mut lmap = LineMap::new();
+            let mut errors = Vec::new();
+            let tokens = token::lex(&path, &buf, &mut lmap, &mut errors);
+            let chunk = syntax::parse(&tokens, &lmap, &mut errors);
+            if is_incomplete(&errors) {
+                continue;
+            }
+
+            // Resolve symbols, compile.
+            let scope_set = scope::resolve(&chunk, &lmap, &mut errors);
+            let package_opt =
+                compile::compile_chunk(name.clone(), &chunk, &scope_set, &lmap, &mut errors);
+            if !errors.is_empty() {
+                for err in &errors {
+                    eprintln!("{}", err);
+                }
+                errors.drain(..);
+                buf.drain(..);
+                continue;
+            }
+            let package = package_opt.unwrap();
+            if args.disassemble {
+                print!(
+                    "-- begin disassembly: {}\n{}\n-- end disassembly --\n",
+                    &package.name, package
+                );
+            }
+
+            // Link and load the package. This will run its initializer, which
+            // is the top-level code.
+            let res =
+                unsafe { PackageLoader::load_given_package(&loader_cell, interp_fac, package) };
+            if let Err(err) = res {
+                eprintln!("{}", err);
+            }
+            buf.drain(..);
+        }
+    }
+
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -72,4 +157,4 @@ impl Display for StringError {
     }
 }
 
-impl Error for StringError {}
+impl std::error::Error for StringError {}
